93e696ca7b4263815e0e38100e63e39ed03e14bf
mr-rohangupta <rggupta.nagpur@gmail.com>
2021-06-01 22:26:08 +0530

    Updated Hystrix Circuit Breaker

--- cloud-gateway/src/main/java/com/java/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/java/cloudgateway/CloudGatewayApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,6 +17,23 @@ public class CloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+
+    @Bean
+    public FallbackProvider departmentFallbackProvider() {
+        GenericFallbackProvider departmentFallbackProvider = new GenericFallbackProvider();
+        departmentFallbackProvider.setRoute("DEPARTMENT-SERVICE");
+        return departmentFallbackProvider;
+    }
+
+    @Bean
+    public FallbackProvider userFallbackProvider() {
+        GenericFallbackProvider userFallbackProvider = new GenericFallbackProvider();
+        userFallbackProvider.setRoute("USER-SERVICE");
+        userFallbackProvider.setRawStatusCode(200);
+        userFallbackProvider.setStatusCode(HttpStatus.OK);
+        userFallbackProvider.setResponseBody("We are little busy. Comeback After Sometime");
+        return userFallbackProvider;
     }
 
 }
--- cloud-gateway/src/main/java/com/java/cloudgateway/FallBackMethodController.java
+++ cloud-gateway/src/main/java/com/java/cloudgateway/FallBackMethodController.java
@@ -3,6 +3,7 @@ package com.java.cloudgateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Deprecated
 @RestController
 public class FallBackMethodController {
 
